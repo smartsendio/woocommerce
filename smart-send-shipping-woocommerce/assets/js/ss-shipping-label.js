91839b718ad35e35a12d73145dfe039429384eb6
Shadi Manna <manna.shadi@gmail.com>
2018-09-05 17:18:33 +0200

    Label JS documentation

--- smart-send-shipping-woocommerce/assets/js/ss-shipping-label.js
+++ smart-send-shipping-woocommerce/assets/js/ss-shipping-label.js
@@ -14,6 +14,7 @@ jQuery(function ($) {
             $('#ss-shipping-label-form .error').remove();
             $('#ss-shipping-label-form .updated').remove();
 
+            // Block metabox while doing AJAX
             $('#ss-shipping-label-form').block({
                 message: null,
                 overlayCSS: {
@@ -30,21 +31,28 @@ jQuery(function ($) {
                 ss_shipping_label_nonce: $('#ss_shipping_label_nonce').val()
             };
 
+            // AJAX call with order data to create label
             $.post(woocommerce_admin_meta_boxes.ajax_url, data, function (response) {
+                // Unblock AJAX
                 $('#ss-shipping-label-form').unblock();
 
+                // Loop through response, could be two if we are creating return label as well as normal label
                 $.each( response, function( key, value ) {
                     
+                    // Add error to metabox if exists
                     if (value.error) {
                          $('#ss-shipping-label-form').append('<div id="ss-shipping-error" class="error ss-meta-message">' + value.error + '</div>'); 
 
                     } else if (value.success.woocommerce) {
-                        //$('.ss_agent_address').html(value.success.agent_address);
+                        
+                        // If return label, place correct link
                         if( value.success.woocommerce.return ) {
                             $('#ss-shipping-label-form').append('<div id="ss-label-created" class="updated ss-meta-message"><a href="' + value.success.woocommerce.label_link + '" target="_blank">' + ss_label_data.download_return_label + '</a></div>');
                         } else {
                             $('#ss-shipping-label-form').append('<div id="ss-label-created" class="updated ss-meta-message"><a href="' + value.success.woocommerce.label_link + '" target="_blank">' + ss_label_data.download_label + '</a></div>');
                         }
+
+                        // Add order note with tracking info
                         if (value.success.woocommerce.order_note) {
 
                             $('#woocommerce-order-notes').block({
@@ -63,6 +71,7 @@ jQuery(function ($) {
                                 security: woocommerce_admin_meta_boxes.add_order_note_nonce
                             };
 
+                            // Order note AJAX call
                             $.post(woocommerce_admin_meta_boxes.ajax_url, data, function (response_note) {
                                 // alert(response_note);
                                 $('ul.order_notes').prepend(response_note);
@@ -83,6 +92,7 @@ jQuery(function ($) {
         },
     }
 
+    // Init object
     ss_shipping_label_items.init();
 
 });
