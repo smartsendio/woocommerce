a2d1f1be0f38a878907d08c477f5c2bf80de6a2e
Anders Bilfeldt <abilfeldt@gmail.com>
2024-01-18 13:55:51 +0100

    Add version number

--- smart-send-logistics/smart-send-logistics.php
+++ smart-send-logistics/smart-send-logistics.php
@@ -6,7 +6,7 @@
  * Author: Smart Send ApS
  * Author URI: https://www.smartsend.io
  * Text Domain: smart-send-logistics
- * Version: 8.0.27
+ * Version: 8.1.0
  * WC requires at least: 3.0.1
  * WC tested up to: 7.9
  *
@@ -34,7 +34,7 @@ if (!class_exists('SS_Shipping_WC')) :
     class SS_Shipping_WC
     {
 
-        private $version = "8.0.27";
+        private $version = "8.1.0";
 
         /**
          * Instance to call certain functions globally within the plugin
